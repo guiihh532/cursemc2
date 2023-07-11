fbeeae4cd20bdf230fc6d61d4a6fb287ba498e81
Guilherme Messias <guiihh532@gmail.com>
2023-07-11 14:47:06 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/gomlek/cursemc/resources/CategoriaResource.java
+++ src/main/java/com/gomlek/cursemc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.gomlek.cursemc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gomlek.cursemc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,7 +15,18 @@ public class CategoriaResource {
 
 	//obtem um dado do metodo
 	@GetMapping
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria> listar() {
+		
+		
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		
+		list.add(cat);
+		list.add(cat2);
+				
+				
+		return list;
 	}
 }
